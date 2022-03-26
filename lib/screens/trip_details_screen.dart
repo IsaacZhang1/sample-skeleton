d363fe7644b0a668de4ea9025a3d5bc7d168a15b
Isaac Zhang <izhang@Isaacs-MacBook-Pro.local>
2022-03-26 09:31:23 -0700

    Adding font

---
+++ lib/screens/trip_details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TripDetailsScreen extends StatelessWidget {
+  const TripDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Trip Details'),
+      ),
+      body: Container(),
+    );
+  }
+}
