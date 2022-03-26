ad2ae0981b6bed12bb70c379a7db67d89b8c3359
Isaac Zhang <izhang@Isaacs-MacBook-Pro.local>
2022-03-25 20:06:03 -0700

    skeleton

---
+++ android/app/src/main/kotlin/com/example/lark_skeleton/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lark_skeleton
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:lark_skeleton/screens/history_screen.dart';
+import 'package:lark_skeleton/screens/shopping_list_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  String _appBarTitle() {
+    if (_selectedIndex == 0) {
+      return 'Shopping List';
+    } else if (_selectedIndex == 1) {
+      return 'History';
+    } else {
+      return 'Default Title';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping list',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(_appBarTitle()),
+          ),
+          body: IndexedStack(
+            index: _selectedIndex,
+            children: const [
+              ShoppingListScreen(),
+              HistoryScreen(),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: 'List'),
+              BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
